7a51f6e1cae539aa6b132c9193ac8a132d20a7ef
Stefan <Stefan@PC-Lederhaas>
2016-02-27 14:56:31 +0100

    new commit

--- src/gameserver/GameServer.java
+++ src/gameserver/GameServer.java
@@ -15,7 +15,7 @@ public class GameServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("server not running");
     }
     
 }
