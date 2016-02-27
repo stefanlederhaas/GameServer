3c0f193a34e9500939bc099d163b3447404017a0
philipp <philipp@philipp>
2016-02-27 15:03:07 +0100

    Enum for the Game Selection added. Author corrected

--- src/gameserver/GameServer.java
+++ src/gameserver/GameServer.java
@@ -7,7 +7,7 @@ package gameserver;
 
 /**
  *
- * @author Stefan
+ * @author Stefan Lederhaas
  */
 public class GameServer {
 
@@ -15,7 +15,7 @@ public class GameServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("server not running");
+        
     }
     
 }
