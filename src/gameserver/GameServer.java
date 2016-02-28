689ba21b5648ec2c3195d463b420ad7d21e251f6
philipp <philipp@philipp>
2016-02-28 10:01:28 +0100

    TicTacToe Server finished Command Class changed to an interface with the cmds as methodes Gamebase empyied TicTacToe Server debugged and tested MainServer started to code

--- src/gameserver/GameServer.java
+++ src/gameserver/GameServer.java
@@ -5,42 +5,100 @@
  */
 package gameserver;
 
-import gameserver.TicTacToe.Tictactoe;
-import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import javax.net.ssl.SSLSocket;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class GameServer {
 
+    private boolean running=true;
+    private LinkedList<Thread> games = new LinkedList<>();
+    
+    public void clear() throws IOException
+    {
+        System.out.print("\033[H\033[2J"); 
+       
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+    
    
+    public void createGame()
+    {
+        System.out.println("New Game");
+    }
+    
+    public void manageGame()
+    {
+        System.out.println("MG");
+    }
+    
+    public void stopServer_S()
+    {
+        System.out.println("SS");
+    }
+    
+    public void killAll()
+    {
+        running = false;
+    }
+    
+    public void exit()
+    {
+        running = false;
+    }
+    
+    public byte printMainMenu()
+    {
+        System.out.println("What would you wish to do?");
+        System.out.println("1) Create new game");
+        System.out.println("2) Manage Running game");
+        System.out.println("3) Stop server(s)");
+        System.out.println("4) Kill'em all");
+        System.out.println("5) Safe exit");
+        System.out.print("Your choice: ");
+        return new Scanner(System.in).nextByte();
+        
+    }
+    
     public static void main(String[] args) {
-        try {
-            Tictactoe ttt = new Tictactoe(1400);
-            Thread t = new Thread(ttt);
-            t.start();
+        GameServer server = new GameServer();
+        do{
+            try {
+                
+                server.clear();
+                byte b = server.printMainMenu();
+                switch(b)
+                {
+                    case 1:
+                        server.createGame();
+                        break;
+                    case 2:
+                        server.manageGame();
+                        break;
+                    case 3:
+                        server.stopServer_S();
+                        break;
+                    case 4:
+                        server.killAll();
+                        break;
+                    case 5:
+                        server.exit();
+                        break;
+                        
+                }
+            } catch (IOException ex) {
+                System.out.println("Clear didn't wort exiting now");
+            }
             
-            Socket s1,s2;
-            s1 = new Socket("localhost", 1400);
-            s2 = new Socket("localhost",1400);
-            
-            BufferedReader br = new BufferedReader(new InputStreamReader(s1.getInputStream()));
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(s2.getInputStream()));
-            
-            
-            
-           while(t.isAlive())
-           {
-               
-               System.out.println("P1: "+br.readLine());
-               System.out.println("P2: "+br2.readLine());
-           }
-            
-        } catch (IOException ex) {
-            
-        }
+        }while(server.isRunning());
     }
     
 }
